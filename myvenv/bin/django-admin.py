69012362f322ae2adf9cffff797c63f81882aae0
Lilyan <lilyan.sica@hummingbirdit.tech>
2020-10-28 00:55:04 -0600

    modificaciones

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lsica/Desktop/proyecto SUBASTA/Backend_proveeedor/proveedor/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
